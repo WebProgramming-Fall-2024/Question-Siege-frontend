9074baa36e42211d9dfdeadd7cd9efff2aa89021
mehrbarzamini <mehrbarzamini@gmail.com>
2024-12-16 21:31:46 +0330

    Refactor: move global.css to src/styles and move fonts from src/lib/fonts to src/assets and update the imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import {Login_signup} from "./component/login_signup/login_signup";
 import {Main} from "./component/Main/Main";
 import 'bootstrap/dist/css/bootstrap.min.css'
-import $ from 'jquery'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import './lib/fonts/icomoon/style.css'
-import './lib/global.css'
-import Swal from "sweetalert2";
-import {createContext, useState} from "react";
+import './assets/fonts/icomoon/style.css'
+import './styles/global.css'
 import {TasksProvider} from "./component/TaskContext/TasksContext.js";
 
 
