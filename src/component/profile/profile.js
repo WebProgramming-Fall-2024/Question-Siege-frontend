4ccd75555e03e502c2f9f5f31463665bd4ae8132
mehrbarzamini <mehrbarzamini@gmail.com>
2024-12-17 17:11:46 +0330

    feat: implement api calls for profile and user

--- src/component/profile/profile.js
+++ src/component/profile/profile.js
@@ -1,247 +1,161 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Modal from "react-bootstrap/Modal";
-import {useState} from "react";
 
+export function Profile() {
+    const location = useLocation();
+    const query = new URLSearchParams(location.search);
+    const username = query.get("name");
 
-function follow(){
+    const [profile, setProfile] = useState({
+        username: "",
+        answeredQuestions: 0,
+        score: 0,
+        designedQuestions: 0,
+    });
 
-}
-export function Profile(){
-    const location = useLocation()
-    const q = new URLSearchParams(location.search)
-    // todo api for get profile 1 user that contain games and question
+    const [games, setGames] = useState([]);
+    const [questions, setQuestions] = useState([]);
+    const [loading, setLoading] = useState(true);
     const [modalShow, setModalShow] = useState(false);
     const [modalShow2, setModalShow2] = useState(false);
 
-    let data = ['item1',"item2","item3","item4","item4"]
+    useEffect(() => {
+        const fetchProfileData = async () => {
+            try {
+                const token = localStorage.getItem("login_token");
+                const response = await axios.get(
+                    `${process.env.REACT_APP_API_URL}/users/profile/details?name=${username}`,
+                    {
+                        headers: { Authorization: token },
+                    }
+                );
+                const data = response.data;
+
+                setProfile({
+                    username: data.username,
+                    answeredQuestions: data.answeredQuestions,
+                    score: data.score,
+                    designedQuestions: data.designedQuestions,
+                });
+
+                setGames(data.games);
+                setQuestions(data.questions);
+            } catch (error) {
+                console.error("Error fetching profile data:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProfileData();
+    }, [username]);
+
+    if (loading) {
+        return <div>در حال بارگذاری...</div>;
+    }
 
     return (
         <div>
-            <div className="my-4 mx-3 d-flex flex-column justify-content-between" style={{direction: "rtl"}}>
+            {/* Follow Button and Profile Details */}
+            <div className="my-4 mx-3 d-flex flex-column justify-content-between" style={{ direction: "rtl" }}>
                 <div className="row justify-content-around">
                     <div className="col-md-3 col-10 row justify-content-between">
-                        <button className=" btn btn-primary my-3 px-4" onClick={follow} style={{width: 'fit-content'}} >دنبال کردن</button>
+                        <button className="btn btn-primary my-3 px-4" onClick={follow} style={{ width: "fit-content" }}>
+                            دنبال کردن
+                        </button>
                     </div>
-                    <div className="col-md-8 col-10 mt-3" style={{overflowX: "auto"}}>
-                        <table className="table table-hover text-center" >
-                            <thead className="table-primary" >
+                    <div className="col-md-8 col-10 mt-3" style={{ overflowX: "auto" }}>
+                        <table className="table table-hover text-center">
+                            <thead className="table-primary">
                             <tr>
-                                <th className="px-4" style={{whiteSpace: "nowrap"}}>نام کاربری</th>
-                                <th className="px-4" style={{whiteSpace: "nowrap"}}>تعداد سوال جواب داده شده</th>
-                                <th className="px-4" style={{whiteSpace: "nowrap"}}>امتیاز</th>
-                                <th className="px-4" style={{whiteSpace: "nowrap"}}>تعداد سوال طرح شده</th>
-
-
-
+                                <th>نام کاربری</th>
+                                <th>تعداد سوال جواب داده شده</th>
+                                <th>امتیاز</th>
+                                <th>تعداد سوال طرح شده</th>
                             </tr>
                             </thead>
                             <tbody>
                             <tr>
-                                <td>mahdiyar</td>
-                                <td>12</td>
-                                <td>24</td>
-                                <td>2</td>
-
+                                <td>{profile.username}</td>
+                                <td>{profile.answeredQuestions}</td>
+                                <td>{profile.score}</td>
+                                <td>{profile.designedQuestions}</td>
                             </tr>
-
                             </tbody>
                         </table>
-
                     </div>
-
-
-
                 </div>
-
             </div>
-            <div style={{border:'1px solid black', borderRadius: "12px"}} className="mx-4">
-                <div className="mx-5 my-2" style={{overflowX:"auto",direction: "rtl"}}>
-                    <h1 style={{textShadow: "1px 1px grey"}}>بازی ها</h1>
-                    <table className="table table-hover text-center mt-2" >
-                        <thead className="table-primary" >
+
+            {/* Games Section */}
+            <div style={{ border: "1px solid black", borderRadius: "12px" }} className="mx-4">
+                <div className="mx-5 my-2" style={{ overflowX: "auto", direction: "rtl" }}>
+                    <h1 style={{ textShadow: "1px 1px grey" }}>بازی ها</h1>
+                    <table className="table table-hover text-center mt-2">
+                        <thead className="table-primary">
                         <tr>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>#</th>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>عنوان سوال</th>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>طراح سوال</th>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>وضعیت</th>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>امتیاز</th>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>عملیات</th>
-
-
+                            <th>#</th>
+                            <th>عنوان سوال</th>
+                            <th>طراح سوال</th>
+                            <th>وضعیت</th>
+                            <th>امتیاز</th>
                         </tr>
                         </thead>
                         <tbody>
-                        {data.map((item,index) => {
-                            return(<tr>
-                                <th>{index+1}</th>
-                                <td>تت ۱</td>
-                                <td>مهرشاد برزمینی</td>
-                                <td><i className="icon-tick-4 text-success my-2"></i> </td>
-                                <td>2</td>
+                        {games.map((game, index) => (
+                            <tr key={index}>
+                                <th>{index + 1}</th>
+                                <td>{game.title}</td>
+                                <td>{game.designer}</td>
                                 <td>
-                                    <div className="dropdown">
-                                        <button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            <i className="icon-cog6"></i>
-
-                                        </button>
-                                        <ul className="dropdown-menu">
-                                            <li><div className='dropdown-item text-center tet' onClick={()=>{setModalShow(true);show_deteial(index)}}>جزییات</div></li>
-
-                                        </ul>
-                                    </div>
+                                    {game.status === "correct" ? (
+                                        <i className="icon-tick-4 text-success my-2"></i>
+                                    ) : (
+                                        <i className="icon-cross-4 text-danger my-2"></i>
+                                    )}
                                 </td>
-
-                            </tr>)
-
-                        })}
-
+                                <td>{game.score}</td>
+                            </tr>
+                        ))}
                         </tbody>
                     </table>
                 </div>
             </div>
 
-
-            <div style={{border:'1px solid black', borderRadius: "12px",marginBottom:"99px"}} className="mx-4 mt-4">
-
-                <div className="mx-5 " style={{overflowX:'auto',direction: "rtl"}}>
-                    <h1 style={{textShadow: "1px 1px grey"}}>سوالات</h1>
-                    <table className="table table-hover text-center" >
-                        <thead className="table-primary" >
+            {/* Questions Section */}
+            <div style={{ border: "1px solid black", borderRadius: "12px", marginBottom: "99px" }} className="mx-4 mt-4">
+                <div className="mx-5 " style={{ overflowX: "auto", direction: "rtl" }}>
+                    <h1 style={{ textShadow: "1px 1px grey" }}>سوالات</h1>
+                    <table className="table table-hover text-center">
+                        <thead className="table-primary">
                         <tr>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>#</th>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>عنوان سوال</th>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>دسته بندی</th>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>تگ</th>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>تاریخ</th>
-                            <th className="px-4" style={{whiteSpace: "nowrap"}}>عملیات</th>
-
-
+                            <th>#</th>
+                            <th>عنوان سوال</th>
+                            <th>دسته بندی</th>
+                            <th>تگ</th>
+                            <th>تاریخ</th>
                         </tr>
                         </thead>
                         <tbody>
-                        {data.map((item,index) => {
-                            return(<tr>
-                                <th>{index+1}</th>
-                                <td>عید۱</td>
-                                <td>ریاضی</td>
-                                <td>ریاضی</td>
-
-                                <td>۱۳۹۷/۱۲/۱۲</td>
-                                <td>
-                                    <div className="dropdown">
-                                        <button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            <i className="icon-cog6"></i>
-
-                                        </button>
-                                        <ul className="dropdown-menu">
-                                            <li><div className='dropdown-item text-center tet' onClick={()=>{setModalShow2(true);show_deteial2(index)}}>جزییات</div></li>
-
-                                        </ul>
-                                    </div>
-                                </td>
-
-                            </tr>)
-
-                        })}
-
-
+                        {questions.map((question, index) => (
+                            <tr key={index}>
+                                <th>{index + 1}</th>
+                                <td>{question.title}</td>
+                                <td>{question.category}</td>
+                                <td>{question.tag}</td>
+                                <td>{new Date(question.date).toLocaleDateString("fa-IR")}</td>
+                            </tr>
+                        ))}
                         </tbody>
                     </table>
                 </div>
             </div>
-
-            <MyVerticallyCenteredModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-            />
-            <MyVerticallyCenteredModal2
-                show={modalShow2}
-                onHide={() => setModalShow2(false)}
-            />
         </div>
-    )
+    );
 
-    function MyVerticallyCenteredModal2(props) {
-        return (
-            <Modal
-                {...props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header className="d-flex flex-row-reverse justify-content-between">
-
-                    <Modal.Title id="contained-modal-title-vcenter">
-
-                        جزئیات سوال
-                    </Modal.Title>
-                    <button type="button" className="close btn" style={{fontSize:"16px"}} onClick={props.onHide}>×</button>
-
-                </Modal.Header>
-                <Modal.Body>
-                    <div className="d-flex justify-content-center align-items-center mt-4">
-                        <div  className="p-4 w-100 d-flex justify-content-center align-items-end flex-column">
-                            <h4>رادیکال ۱6 چند می شود ؟</h4>
-                            <div className="w-100 text-end px-2 py-4 my-2" style={{border: "2px solid grey",boxShadow: "2px 2px grey",borderRadius: "10px"}}>1</div>
-                            <div className="w-100 text-end px-2 py-4 my-2" style={{border: "2px solid grey",boxShadow: "2px 2px grey",borderRadius: "10px"}}>2</div>
-                            <div className="w-100 text-end px-2 py-4 my-2" style={{border: "2px solid grey",boxShadow: "2px 2px grey",borderRadius: "10px"}}>3</div>
-                            <div className="w-100 text-end px-2 py-4 my-2" style={{border: "2px solid green",boxShadow: "2px 2px grey",borderRadius: "10px"}}>4</div>
-
-
-                        </div>
-
-                    </div>
-
-                </Modal.Body>
-                <Modal.Footer>
-                    <button className="btn btn-outline-success" onClick={props.onHide}>بستن</button>
-                </Modal.Footer>
-            </Modal>
-        );
-    }
-    function show_deteial2(index){
-        console.log(index)
-    }
-
-    function MyVerticallyCenteredModal(props) {
-        return (
-            <Modal
-                {...props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header className="d-flex flex-row-reverse justify-content-between">
-
-                    <Modal.Title id="contained-modal-title-vcenter">
-
-                        جزئیات سوال
-                    </Modal.Title>
-                    <button type="button" className="close btn" style={{fontSize:"16px"}} onClick={props.onHide}>×</button>
-
-                </Modal.Header>
-                <Modal.Body>
-                    <div className="d-flex justify-content-center align-items-center mt-4">
-                        <div  className="p-4 w-100 d-flex justify-content-center align-items-end flex-column">
-                            <h4>رادیکال ۱6 چند می شود ؟</h4>
-                            <div className="w-100 text-end px-2 py-4 my-2" style={{border: "2px solid grey",boxShadow: "2px 2px grey",borderRadius: "10px"}}>1</div>
-                            <div className="w-100 text-end px-2 py-4 my-2" style={{border: "2px solid red",boxShadow: "2px 2px grey",borderRadius: "10px"}}>2</div>
-                            <div className="w-100 text-end px-2 py-4 my-2" style={{border: "2px solid grey",boxShadow: "2px 2px grey",borderRadius: "10px"}}>3</div>
-                            <div className="w-100 text-end px-2 py-4 my-2" style={{border: "2px solid green",boxShadow: "2px 2px grey",borderRadius: "10px"}}>4</div>
-
-
-                        </div>
-
-                    </div>
-
-                </Modal.Body>
-                <Modal.Footer>
-                    <button className="btn btn-outline-success" onClick={props.onHide}>بستن</button>
-                </Modal.Footer>
-            </Modal>
-        );
-    }
-    function show_deteial(index){
-        console.log(index)
+    function follow() {
+        console.log("Follow function not implemented.");
     }
 }
--- src/component/user/user.js
+++ src/component/user/user.js
@@ -1,93 +1,131 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export function User(){
-    let data = ['item1','item2','item3','item4']
-    // todo api for this user
-    return(
+export function User() {
+    const [users, setUsers] = useState([]); // State to store users fetched from the API
+    const [loading, setLoading] = useState(true); // Loading state
+
+    // Fetch users on component mount
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const token = localStorage.getItem("login_token"); // Retrieve token
+                const response = await axios.get(`${process.env.REACT_APP_API_URL}/users`, {
+                    headers: { Authorization: token },
+                });
+                setUsers(response.data); // Set the user data
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching users:", error);
+                setLoading(false);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    return (
         <div>
-            <div className="my-4 mx-3 d-flex flex-column justify-content-between" style={{direction: 'rtl'}}>
+            {/* Search and Filter Section */}
+            <div className="my-4 mx-3 d-flex flex-column justify-content-between" style={{ direction: "rtl" }}>
                 <div className="collapse" id="navbarToggleExternalContent">
                     <div className="p-4 row justify-content-around">
                         <div className="col-md-5">
-                            <label htmlFor="category_search" className="my-2 fw-bolder">نام دسته بندی</label>
-                            <input type="text" className="form-control" id="category_search" placeholder="نام دسته بندی مورد نظر خود را وارد کنید" autoComplete="off" />
-
+                            <label htmlFor="category_search" className="my-2 fw-bolder">نام بازیکن</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="category_search"
+                                placeholder="نام بازیکن را وارد کنید"
+                                autoComplete="off"
+                            />
                         </div>
                         <div className="col-md-5">
-                            <label htmlFor="category_search" className="my-2 fw-bolder">فیلتر</label>
-                            <select className="form-control">
+                            <label htmlFor="category_filter" className="my-2 fw-bolder">فیلتر</label>
+                            <select className="form-control" id="category_filter">
                                 <option>انتخاب کنید</option>
                                 <option>نام</option>
                                 <option>امتیاز</option>
                                 <option>تعداد سوال طرح شده</option>
-
-
                             </select>
                         </div>
                         <div className="mt-2 d-flex justify-content-end">
                             <button className="btn btn-outline-primary">اعمال تغییرات</button>
-
                         </div>
-
                     </div>
                 </div>
                 <div className="d-flex justify-content-end">
-
-
                     <nav className="navbar">
                         <div className="container-fluid">
-                            <button className="btn btn-outline-success" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <button
+                                className="btn btn-outline-success"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#navbarToggleExternalContent"
+                                aria-controls="navbarToggleExternalContent"
+                                aria-expanded="false"
+                                aria-label="Toggle navigation"
+                            >
                                 جستجو
                             </button>
                         </div>
                     </nav>
                 </div>
-
             </div>
-            <div className="mx-5 text-center" style={{overflowX:'auto',direction: "rtl"}}>
-                <table className="table table-hover" >
-                    <thead className="table-primary" >
-                    <tr>
-                        <th className="px-4" style={{whiteSpace: "nowrap"}}>#</th>
-                        <th className="px-4" style={{whiteSpace: "nowrap"}}>نام بازیکن</th>
-                        <th className="px-4" style={{whiteSpace: "nowrap"}}>تاریخ عضویت</th>
-                        <th className="px-4" style={{whiteSpace: "nowrap"}}>تعداد سوال طرح شده</th>
-                        <th className="px-4" style={{whiteSpace: "nowrap"}}>امتیاز</th>
-                        <th className="px-4" style={{whiteSpace: "nowrap"}}>جزییات</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {data.map((item,index) => {
-                        return(<tr>
-                            <th>{index+1}</th>
-                            <td>مهرشاد برزمینی</td>
-                            <td>۱۳۰۰/۱۰/۱</td>
-                            <td>4</td>
-                            <td>56</td>
-                            <td>
-                                <div className="dropdown">
-                                    <button className="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        <i className="icon-cog6"></i>
 
-                                    </button>
-                                    <ul className="dropdown-menu">
-                                        <div className="d-flex justify-content-center">
-                                            <Link to='../profile?name=salam' className="mx-auto align-self-center">جزییات</Link>
-
-                                        </div>
-
-                                    </ul>
-                                </div>
-                            </td>
-
-
-                        </tr>)
-
-                    })}
-
-                    </tbody>
-                </table>
+            {/* Users Table */}
+            <div className="mx-5 text-center" style={{ overflowX: "auto", direction: "rtl" }}>
+                {loading ? (
+                    <div>در حال بارگذاری...</div>
+                ) : (
+                    <table className="table table-hover">
+                        <thead className="table-primary">
+                        <tr>
+                            <th>#</th>
+                            <th>نام بازیکن</th>
+                            <th>تاریخ عضویت</th>
+                            <th>تعداد سوال طرح شده</th>
+                            <th>امتیاز</th>
+                            <th>جزییات</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {users.map((user, index) => (
+                            <tr key={user.id}>
+                                <th>{index + 1}</th>
+                                <td>{user.username}</td>
+                                <td>{new Date(user.signupDate).toLocaleDateString("fa-IR")}</td>
+                                <td>{user.designedQuestions}</td>
+                                <td>{user.score}</td>
+                                <td>
+                                    <div className="dropdown">
+                                        <button
+                                            className="btn dropdown-toggle"
+                                            type="button"
+                                            data-bs-toggle="dropdown"
+                                            aria-expanded="false"
+                                        >
+                                            <i className="icon-cog6"></i>
+                                        </button>
+                                        <ul className="dropdown-menu">
+                                            <div className="d-flex justify-content-center">
+                                                <Link
+                                                    to={`../profile?name=${user.username}`}
+                                                    className="mx-auto align-self-center"
+                                                >
+                                                    جزییات
+                                                </Link>
+                                            </div>
+                                        </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                )}
             </div>
         </div>
-    )
+    );
 }
